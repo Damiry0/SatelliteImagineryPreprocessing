e412d668b74d968106372018c0ac9890f969b266
Damian Ryś <damian.rys0@gmail.com>
2023-10-21 17:44:51 +0200

    Basic saving to file

--- SatelliteImagineryPreprocessing/Program.cs
+++ SatelliteImagineryPreprocessing/Program.cs
@@ -1,12 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Buffers;
+using System.Diagnostics;
 using TiffLibrary;
+using TiffLibrary.PixelFormats;
 
 
 var workingDirectory = Environment.CurrentDirectory;
 var projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
 
-await using TiffFileReader tiff = await TiffFileReader.OpenAsync(projectDirectory+ @"\Examples\NmTHIR115-1T.N6.asc.1975.313.21.09.16.1929.2020.159.tif");
+var sw = new Stopwatch();
+sw.Start();
 
 
-Console.WriteLine("Hello, World!");
+await using var tiff = await TiffFileReader.OpenAsync(projectDirectory+ @"\Examples\large_file.tif");
+
+using var fieldReader = await tiff.CreateFieldReaderAsync();
+var ifd = await tiff.ReadImageFileDirectoryAsync();
+var tagReader = new TiffTagReader(fieldReader, ifd);
+
+// Get offsets to the strip/tile data
+TiffValueCollection<ulong> offsets, byteCounts;
+if (ifd.Contains(TiffTag.TileOffsets))
+{
+    offsets = await tagReader.ReadTileOffsetsAsync();
+    byteCounts = await tagReader.ReadTileByteCountsAsync();
+}
+else if (ifd.Contains(TiffTag.StripOffsets))
+{
+    offsets = await tagReader.ReadStripOffsetsAsync();
+    byteCounts = await tagReader.ReadStripByteCountsAsync();
+}
+else
+{
+    throw new InvalidDataException("This TIFF file is neither striped or tiled.");
+}
+if (offsets.Count != byteCounts.Count)
+{
+    throw new InvalidDataException();
+}
+
+// Extract strip/tile data
+using var contentReader = await tiff.CreateContentReaderAsync();
+int count = offsets.Count;
+for (int i = 0; i < count; i++)
+{
+    var offset = (long)offsets[i];
+    var byteCount = (int)byteCounts[i];
+    var data = ArrayPool<byte>.Shared.Rent(byteCount);
+    try
+    {
+        await contentReader.ReadAsync(offset, data.AsMemory(0, byteCount));
+        await using var fs = new FileStream(  projectDirectory+ $@"\Results\extracted-{i}.dat", FileMode.Create, FileAccess.Write);
+        await fs.WriteAsync(data, 0, byteCount);
+    }
+    finally
+    {
+        ArrayPool<byte>.Shared.Return(data);
+    }
+}
+
+sw.Stop();
+
+
+Console.WriteLine("Elapsed={0}",sw.Elapsed);
+
+
+
+
+
